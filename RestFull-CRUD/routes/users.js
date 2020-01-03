256a616c2eb481ed1f8cd24faf4cae6093f90f4c
GiulianiViviana <giuliani.viviana@iisgalvanimi.edu.it>
2020-01-03 13:18:28 +0000

    Esercizio finito

--- RestFull-CRUD/routes/users.js
+++ RestFull-CRUD/routes/users.js
@@ -4,10 +4,10 @@ var router = express.Router();
 const sql = require('mssql')
 
 const config = {
-  user: '4dd_14',  //Vostro user name
+  user: '4DD_14',  //Vostro user name
   password: 'xxx123##', //Vostra password
-  server: "213.140.22.237\\sqlexpress",  //Stringa di connessione
-  database: '4dd_14', //(Nome del DB)
+  server: "213.140.22.237",  //Stringa di connessione
+  database: '4DD_14', //(Nome del DB)
 }
 
 /* GET users listing. */
@@ -17,20 +17,20 @@ router.get('/', function(req, res, next) {
     
     // Query
     let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
-    sqlRequest.query('select * from school.Person', (err, result) => {
+    sqlRequest.query('select * from dbo.[cr-unit-attributes]', (err, result) => {
         if (err) console.log(err); // ... error checks
         res.send(result);  //Invio il risultato
     });
   });
 });
 
-router.get('/search/:name', function(req, res, next) {
+router.get('/search/:name/', function(req, res, next) {
   sql.connect(config, err => {
     // ... error check
     if(err) console.log(err);
     // Query
     let sqlRequest = new sql.Request();
-    sqlRequest.query(`select * from [School].[Person] where FirstName = '${req.params.name}'`, (err, result) => {
+    sqlRequest.query(`select * from dbo.[cr-unit-attributes] where unit = '${req.params.name}'`, (err, result) => {
         // ... error checks
         if (err) console.log(err);
 
