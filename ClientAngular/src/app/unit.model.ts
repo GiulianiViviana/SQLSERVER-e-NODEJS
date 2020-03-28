5bfb667b8bb0d4146055c89d96049c979635ff84
GiulianiViviana <giuliani.viviana@iisgalvanimi.edu.it>
2020-03-28 16:24:38 +0000

    Inserito client angular

---
+++ ClientAngular/src/app/unit.model.ts
@@ -0,0 +1,13 @@
+export class Unit
+{
+    Unit : string;
+    Cost : string;
+    Hit_Speed: string;
+    Deploy_Time: string;
+    Range: string;
+    Target: string;
+    Count: string;
+    Transport: string;
+    Type: string;
+    Rarity: string;
+}
