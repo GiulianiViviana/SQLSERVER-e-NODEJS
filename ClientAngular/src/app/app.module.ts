8bb925518647b83aa3c2982058ef0f6f4e0fc6ed
GiulianiViviana <giuliani.viviana@iisgalvanimi.edu.it>
2020-03-28 21:44:20 +0000

    Inserita grafica

--- ClientAngular/src/app/app.module.ts
+++ ClientAngular/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http'; //Importa Il modulo http
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
  declarations: [
    AppComponent,
  ],
  imports: [
-   BrowserModule, HttpClientModule //Aggiungi il modulo http qui
+   BrowserModule, HttpClientModule, NgbModule //Aggiungi il modulo http qui
  ],
  providers: [],
  bootstrap: [AppComponent]
